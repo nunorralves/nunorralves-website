20a142c6ca503e1a29f68864ebfff8e1afdb4488
nunorralves <nunorralves@gmail.com>
2020-10-04 15:04:31 +0100

    fix firebase env parsing

--- src/lib/db-admin.ts
+++ src/lib/db-admin.ts
@@ -38,7 +38,7 @@ try {
   admin.initializeApp({
     credential: admin.credential.cert({
       clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
+      privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID
     }),
     databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL
