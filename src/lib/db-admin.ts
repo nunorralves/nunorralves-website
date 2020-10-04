6a2b759d1eb2613586cc1a020c7f1417a1e85671
nunorralves <nunorralves@gmail.com>
2020-10-04 01:10:26 +0100

    Adapt firebase to vercel deploy

--- src/lib/db-admin.ts
+++ src/lib/db-admin.ts
@@ -1,11 +1,16 @@
 import * as admin from 'firebase-admin';
-import { join } from 'path';
+// import { join } from 'path';
 // const cert = join(__dirname, '../service-account.json');
-const cert = join(process.cwd(), '/nextblog-firebase.json');
+// const cert = join(process.cwd(), '/nextblog-firebase.json');
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(cert),
+    // credential: admin.credential.cert(cert),
+    credential: admin.credential.cert({
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY,
+      projectId: 'nextblog-6aa55'
+    }),
     databaseURL: 'https://nextblog-6aa55.firebaseio.com'
   });
 }
