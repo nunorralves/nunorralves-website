6bb4499ca2d2a26001f8bda64f4a4a2ba81c4078
nunorralves <nunorralves@gmail.com>
2020-10-05 15:15:56 +0100

    Add line numbers to CodeBlock and fix mdx image max width

--- src/MDXComponents/index.tsx
+++ src/MDXComponents/index.tsx
@@ -6,7 +6,10 @@ const MDXComponents = {
   // h1: props => <h1 style={{ color: 'tomato' }} {...props} />,
   // h1: props => <StyledH1 {...props} />,
   // code: StyledCode,
-  code: props => <CodeBlock {...props} />
+  code: props => <CodeBlock {...props} />,
+  img: props => (
+    <img style={{ maxWidth: '100%', borderRadius: '6px' }} {...props} />
+  )
 };
 
 export default MDXComponents;
--- src/components/CodeBlock/index.tsx
+++ src/components/CodeBlock/index.tsx
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import Highlight, { defaultProps } from 'prism-react-renderer';
 import theme from 'prism-react-renderer/themes/vsDark';
+import { Line, LineContent, LineNo } from './styles';
 
 const CodeBlock = ({ children, className }) => {
   const language = className.replace(/language-/, '');
@@ -14,13 +15,24 @@ const CodeBlock = ({ children, className }) => {
       language={language}
     >
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={{ ...style, padding: '20px' }}>
+        <pre
+          className={className}
+          style={{ ...style, padding: '20px', borderRadius: '6px' }}
+        >
           {tokens.map((line, i) => (
-            <div key={i} {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span key={key} {...getTokenProps({ token, key })} />
-              ))}
-            </div>
+            // <div key={i} {...getLineProps({ line, key: i })}>
+            //   {line.map((token, key) => (
+            //     <span key={key} {...getTokenProps({ token, key })} />
+            //   ))}
+            // </div>
+            <Line key={i} {...getLineProps({ line, key: i })}>
+              <LineNo>{i + 1}</LineNo>
+              <LineContent>
+                {line.map((token, key) => (
+                  <span key={key} {...getTokenProps({ token, key })} />
+                ))}
+              </LineContent>
+            </Line>
           ))}
         </pre>
       )}
