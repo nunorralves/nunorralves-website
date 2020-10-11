f0a04ac10115cc330788ecf8e45786716c882a61
nunorralves <nunorralves@gmail.com>
2020-10-11 23:03:10 +0100

    remove test for staging deployment

--- site.config.ts
+++ site.config.ts
@@ -5,7 +5,7 @@ export default {
   default_publisher_logo: '',
   default_locale: 'en_US',
   site_year: '2020',
-  site_name: 'Teste 2',
+  site_name: 'Nuno Alves',
   site_logo: '/favicons/favicon-32x32.png',
   twitter_username: '@nunorralves',
   linkedin: 'https://www.linkedin.com/in/nralves/',
