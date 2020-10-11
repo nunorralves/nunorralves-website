10049f7705bf5b36f23427cd1b230046f78d97c1
nunorralves <nunorralves@gmail.com>
2020-10-11 23:35:34 +0100

    Correct Site Image

--- site.config.ts
+++ site.config.ts
@@ -1,11 +1,11 @@
 export default {
   url: process.env.PUBLIC_URL,
   title: 'Nuno Alves',
-  image: process.env.PUBLIC_URL + '/favicons/android-chrome-192x192.png',
+  image: process.env.PUBLIC_URL + '/images/siteimage.png',
   default_publisher_logo: '',
   default_locale: 'en_US',
   site_year: '2020',
-  site_name: 'New Teste',
+  site_name: 'Nuno Alves',
   site_logo: '/favicons/favicon-32x32.png',
   twitter_username: '@nunorralves',
   linkedin: 'https://www.linkedin.com/in/nralves/',
